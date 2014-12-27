c3877bb1a1c7b2ccf39f2d460bdb464a159f4fb1
Brandon Carag <brandon.carag@gmail.com>
2014-12-27 10:31:50 -0800

    Added breadth first traversal to binary tree

--- binary_tree.rb
+++ binary_tree.rb
@@ -15,6 +15,38 @@ class Node
   end
 end
 
+class Queue
+  attr_accessor :data_store
+
+  def initialize
+    @data_store = []
+  end
+
+  def enqueue(item)
+    @data_store.push(item)
+  end
+
+  def dequeue
+    @data_store.shift
+  end
+
+  def display 
+    @data_store
+  end
+
+  def empty?
+    @data_store.size == 0
+  end
+
+  def size
+    @data_store.size
+  end
+
+  def head
+    @data_store[0]
+  end
+end
+
 class Tree
   attr_accessor :root_pointer
 
@@ -101,7 +133,55 @@ def depth_first_search(value,node)
   end
 end
 
+def max_depth
+  max_depth = 0
+
 end
+
+def get_height(node)
+  if node == nil
+    return 0
+  end
+
+  [ get_height(node.left_node), get_height(node.right_node) ].max + 1
+end
+
+def is_balanced?(node)
+  if root == nil 
+    return true #Base Case
+  end
+
+  height_diff = get_height(root.left_node) - get_height(root.right)
+  if height_diff.abs > 1 
+    return false
+  else
+    return is_balanced(root.left_node) && is_balanced(root.right_node)
+  end
+
+end
+
+#========= Queue is used as a supporting data structure for level_order_traversal
+
+
+def level_order_traversal(node)
+  return nil if node.nil?
+
+  queue = Queue.new
+  queue.enqueue(node)
+
+  while queue.empty? == false
+    queue.enqueue(node.left_node) if node.left_node != nil
+    queue.enqueue(node.right_node) if node.right_node != nil
+    puts node.value
+    queue.dequeue
+    node = queue.head
+  end
+  
+end
+
+end
+
+#==========
 
 tree = Tree.new(Node.new(7))
 tree.insert(Node.new(3))
@@ -114,6 +194,10 @@ tree.post_order_traversal(tree.root_pointer)
 tree.pre_order_traversal(tree.root_pointer)
 tree.depth_first_search(1,tree.root_pointer)
 tree.depth_first_search(100,tree.root_pointer)
+puts tree.get_height(tree.root_pointer)
+
+tree.level_order_traversal(tree.root_pointer)
+
 
 def find_lowest_common_ancestor(node1,node2)
   node1_height = find_height(node1)
